54897f235e82e7c883f2bbe6a03a94395ddc0e9d
Made By IToncek <toncek2019@post.cz>
2023-03-18 19:42:40 +0100

    Copyrightin'
    Signed-off-by: Made By IToncek <toncek2019@post.cz>

--- app/src/androidTest/java/cz/iqlandia/iqplanetarium/starshiptoolsclient/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cz/iqlandia/iqplanetarium/starshiptoolsclient/ExampleInstrumentedTest.java
@@ -1,13 +1,25 @@
+/*
+ * Copyright (c) 2023 - IToncek
+ *
+ * All rights to modifying this source code are granted, except for changing licence.
+ * Any and all products generated from this source code must be shared with a link
+ * to the original creator with clear and well-defined mention of the original creator.
+ * This applies to any lower level copies, that are doing approximately the same thing.
+ * If you are not sure, if your usage is within these boundaries, please contact the
+ * author on their public email address.
+ */
+
 package cz.iqlandia.iqplanetarium.starshiptoolsclient;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
--- app/src/test/java/cz/iqlandia/iqplanetarium/starshiptoolsclient/ExampleUnitTest.java
+++ app/src/test/java/cz/iqlandia/iqplanetarium/starshiptoolsclient/ExampleUnitTest.java
@@ -1,8 +1,19 @@
+/*
+ * Copyright (c) 2023 - IToncek
+ *
+ * All rights to modifying this source code are granted, except for changing licence.
+ * Any and all products generated from this source code must be shared with a link
+ * to the original creator with clear and well-defined mention of the original creator.
+ * This applies to any lower level copies, that are doing approximately the same thing.
+ * If you are not sure, if your usage is within these boundaries, please contact the
+ * author on their public email address.
+ */
+
 package cz.iqlandia.iqplanetarium.starshiptoolsclient;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +21,8 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-	@Test
-	public void addition_isCorrect() {
-		assertEquals(4, 2 + 2);
-	}
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
